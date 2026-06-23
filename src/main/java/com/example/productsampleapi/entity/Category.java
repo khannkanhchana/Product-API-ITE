7629e5000a918683ffda39188f43ea44e622f2ff
khannkanhchana <kanhchanakhann9@gmail.com>
2026-06-23 17:21:12 +0700

    push without env

--- src/main/java/com/example/productsampleapi/entity/Category.java
+++ src/main/java/com/example/productsampleapi/entity/Category.java
@@ -3,6 +3,7 @@ package com.example.productsampleapi.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
@@ -11,15 +12,26 @@ import java.util.List;
 @Setter
 @ToString
 @Entity(name = "category_tbl")
+@Table(name = "category_tbl")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String description;
-    private Boolean isDeleted;
+    private Boolean isDeleted = false;
+    private String icon; // store icon url
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_category_id")
+    private Category parentCategory;
 
     // one category can have many products
     @OneToMany(mappedBy = "category")
     private List<Product> products;
+
+
+    @OneToMany(mappedBy = "parentCategory")
+    private List<Category> subCategories = new ArrayList<>();
 }
--- src/main/java/com/example/productsampleapi/repository/CategoryRepositoryOld.java
+++ src/main/java/com/example/productsampleapi/repository/CategoryRepositoryOld.java
@@ -1,53 +1,53 @@
-package com.example.productsampleapi.repository;
-
-
-import com.example.productsampleapi.entity.Category;
-import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-
-@Repository
-public class CategoryRepositoryOld {
-    private List<Category> categoryList = new ArrayList<>() {{
-        add(new Category(1, "Beverages", "Soft drinks and beverages", true, null));
-        add(new Category(2, "Snacks", "Chips and snack products",true, null));
-        add(new Category(3, "Dairy", "Milk and dairy products", true, null));
-        add(new Category(4, "Frozen Food", "Frozen food products", true, null));
-    }};
-
-    public List<Category> getAllCategory() {
-        return categoryList;
-    }
-
-    public Category createCategory(Category category) {
-        categoryList.add(category);
-        return category;
-    }
-
-    public Category findCategoryById(Integer id) {
-        return categoryList.stream()
-                .filter(category -> category.getId() == id)
-                .findFirst()
-                .orElseThrow(() ->
-                        new NoSuchElementException("Category with this ID: " + id + " not found"));
-    }
-
-    public boolean deleteCategoryById(Integer id) {
-        return categoryList
-                .removeIf(category -> category.getId() == id);
-    }
-
-    public Category updateCategoryById(Category updateCategory) {
-        for (int i = 0; i < categoryList.size(); i++) {
-            var category = categoryList.get(i);
-
-            if (category.getId() == updateCategory.getId()) {
-                categoryList.set(i, updateCategory);
-                return updateCategory;
-            }
-        }
-        return null;
-    }
-}
+//package com.example.productsampleapi.repository;
+//
+//
+//import com.example.productsampleapi.entity.Category;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.NoSuchElementException;
+//
+//@Repository
+//public class CategoryRepositoryOld {
+//    private List<Category> categoryList = new ArrayList<>() {{
+//        add(new Category(1, "Beverages", "Soft drinks and beverages", true, null));
+//        add(new Category(2, "Snacks", "Chips and snack products",true, null));
+//        add(new Category(3, "Dairy", "Milk and dairy products", true, null));
+//        add(new Category(4, "Frozen Food", "Frozen food products", true, null));
+//    }};
+//
+//    public List<Category> getAllCategory() {
+//        return categoryList;
+//    }
+//
+//    public Category createCategory(Category category) {
+//        categoryList.add(category);
+//        return category;
+//    }
+//
+//    public Category findCategoryById(Integer id) {
+//        return categoryList.stream()
+//                .filter(category -> category.getId() == id)
+//                .findFirst()
+//                .orElseThrow(() ->
+//                        new NoSuchElementException("Category with this ID: " + id + " not found"));
+//    }
+//
+//    public boolean deleteCategoryById(Integer id) {
+//        return categoryList
+//                .removeIf(category -> category.getId() == id);
+//    }
+//
+//    public Category updateCategoryById(Category updateCategory) {
+//        for (int i = 0; i < categoryList.size(); i++) {
+//            var category = categoryList.get(i);
+//
+//            if (category.getId() == updateCategory.getId()) {
+//                categoryList.set(i, updateCategory);
+//                return updateCategory;
+//            }
+//        }
+//        return null;
+//    }
+//}
